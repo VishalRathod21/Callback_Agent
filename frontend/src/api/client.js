312d3904775711561c24a15aa5cd6978386de40b
Vishal Rathod <vishalrathod234509@gmail.com>
2026-07-19 16:18:27 +0530

    Fix API URL construction by removing trailing slash

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -4,7 +4,7 @@ const getApiUrl = () => {
   const envUrl = import.meta.env.VITE_API_BASE_URL;
 
   if (envUrl) {
-    return envUrl.replace(/\/$/, '') + "/api";
+  return envUrl.replace(/\/$/, "");
   }
 
   if (typeof window !== "undefined") {
